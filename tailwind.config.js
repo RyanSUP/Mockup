fcdea548e531ffca23bf7da87620c9b97b296942
RMo <morici23@gmail.com>
2022-05-27 13:51:00 -0400

    stub landing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'stockcar-hero': "url('../images/stockcar-hero.jpg')",
+      }
+    },
   },
   plugins: [],
 }
